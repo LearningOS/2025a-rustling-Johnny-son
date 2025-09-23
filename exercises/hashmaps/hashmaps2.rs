a2335bb37c699d26da8ffab6f29e5b43e01a1216
qx <1428213180@qq.com>
2025-09-23 19:25:58 +0800

    update

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -37,9 +37,9 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Insert new fruits if they are not already present in the
-        // basket. Note that you are not allowed to put any type of fruit that's
-        // already present!
+        if !basket.contains_key(&fruit) {
+            basket.insert(fruit, 3); // 插入至少 1 个，保证总数大于 11
+        }
     }
 }
 
